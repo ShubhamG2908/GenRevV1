dec9d52fb2ab004cd489dc2653492d3d1f45bbd4
Dhruvesh Yadav <dhruveshy@itpathsolutions.co.in>
2025-06-27 14:06:43 +0530

    partial commit.

--- src/Genrev.Web/App/CRM/CRMController.cs
+++ src/Genrev.Web/App/CRM/CRMController.cs
@@ -56,12 +56,16 @@ namespace Genrev.Web.App.CRM
                 ViewBag.SelectedSalesPersonId = selectedSalesPersonId;
 
                 // Get customers assigned to the selected salesperson
-                List<CustomerDDLVM> customers = _customerDataservice.GetCustomerListItemsByPersonnelId(selectedSalesPersonId);
-                ViewBag.Customers = customers;
+                List<CustomerDDLVM> customers = _customerDataservice.GetCustomerListItemsByPersonnelId(selectedSalesPersonId);                
 
                 // Select the CRM record's CustomerId or fallback to the first available customer
                 int selectedCustomerId = customers.Any() ? crmDetails.CustomerId : 0;
                 ViewBag.SelectedCustomerId = selectedCustomerId;
+                foreach (var item in customers)
+                {
+                    item.Selected = item.ID == selectedCustomerId;
+                }                
+                ViewBag.Customers = customers;
                 crmDetails.Strategy = _crmService.GetStrategyByCustomerId(crmDetails.CustomerId);
                 crmDetails.UploadedFiles = _crmService.GetFilesByCRMId(crmDetails.Id);
 
